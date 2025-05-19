d7905cbc57d1a33ffc7cd875364611c807146f13
Amulya Saini <109570286+amulya-saini@users.noreply.github.com>
2025-05-19 11:52:39 -0500

    Update Step04_cellranger.count.sh

--- cellranger/Step04_cellranger.count.sh
+++ cellranger/Step04_cellranger.count.sh
@@ -1,7 +1,7 @@
 cellranger-atac count \
   --id=SRR24326672_output \
-  --reference=/home/lab_mokalled/Amulya/Reference_genome_cellranger/refdata-cellranger-arc-GRCm39-2024-A \
-  --fastqs=/home/lab_mokalled/Amulya/scATAC/SRR24326672 \
+  --reference=\Amulya/Reference_genome_cellranger/refdata-cellranger-arc-GRCm39-2024-A \
+  --fastqs=\Amulya/scATAC/SRR24326672 \
   --sample=ATAC_UN_6 \
   --localcores=16 \
   --localmem=150
